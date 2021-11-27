d11a62e80d28cc4b3a8cf26843c49eb718c5cc3e
Avillameza <villa.k.ana@gmail.com>
2021-11-27 13:29:48 -0800

    Created the Topic model with id, name, desc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_194024) do
+ActiveRecord::Schema.define(version: 2021_11_27_211902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2021_05_27_194024) do
     t.string "title", null: false
     t.string "description"
     t.string "thumbnail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "topics", id: :string, force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
